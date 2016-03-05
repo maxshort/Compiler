6c1a818d4089fe43ab4669893aa2d542bf73dee3
maxshort <maxshort@users.noreply.github.com>
2016-03-05 15:46:56 -0600

    Can now push automatically to remote server on successful build

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,25 @@
+var fs = require('fs');
 var gulp = require("gulp");
 var merge = require("merge-stream");
 var shell = require('gulp-shell');
+var GulpSSH = require('gulp-ssh');
 
+gulp.task("pushOnSuccess", ['build'], function() {
+	try {
+		var rawCreds = fs.readFileSync("./cred.json", "utf-8");
+		var creds = JSON.parse(rawCreds);
+		var gulpSSH = new GulpSSH({
+			ignoreErrors:false,
+			sshConfig:creds
+		});
+		
+		return gulp.src("./build/*")
+		.pipe(gulpSSH.dest("./public_html/compiler"));
+	}
+	catch(e) {
+		throw "Error reading credentials: " + e;
+	}
+});
 
 gulp.task('build', ['makeParser']);
 
